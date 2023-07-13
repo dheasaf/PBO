b0f7e3199da298f8af1850098531dd53036801dd
Dhea safira <127371134+dheasaf@users.noreply.github.com>
2023-07-13 15:02:56 +0700

    Update testExceptions.java

--- PBO-TEORI/src/latihan_12/testExceptions.java
+++ PBO-TEORI/src/latihan_12/testExceptions.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Melida070723;
+package latihan_12;
 
 /**
  *
- * @author Melida Sari
+ * @author dhea safira
  */
 public class TestExceptions {
     public static void main(String[] args) {
