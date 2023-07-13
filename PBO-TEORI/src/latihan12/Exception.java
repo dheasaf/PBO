d77e8999ee4594006ce9b9d5ea7c0a2c77206fd8
Dhea safira <127371134+dheasaf@users.noreply.github.com>
2023-07-13 14:52:11 +0700

    upload modul 12

--- PBO-TEORI/src/latihan12/Exception.java
+++ PBO-TEORI/src/latihan12/Exception.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package latihan12;
+package Melida070723;
 
 /**
  *
- * @author dhea safira
+ * @author Melida Sari
  */
 public class Exception {
     public static void main(String[] args) {
